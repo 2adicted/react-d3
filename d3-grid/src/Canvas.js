e337826c677ed2f873fa51d4ae343f733ffba9de
Deyan Nenov <dnenov@archilizer.com>
2020-10-24 22:15:27 +0100

    great! pixelization works

--- d3-grid/src/Canvas.js
+++ d3-grid/src/Canvas.js
@@ -44,6 +44,7 @@ class Canvas extends React.Component {
   constructor(){
     super()    
     this.canvas = React.createRef()
+    this.secondCanvas = React.createRef()
     this.image = React.createRef()
     this.state = {
       canvasWidth: 401,
@@ -52,7 +53,9 @@ class Canvas extends React.Component {
       gridHeight: 80,
       gridRes: 10,
       picture: "https://i.ytimg.com/vi/wvCGaWXr8Qk/sddefault.jpg",
-      imgData: ""
+      imgData: "",
+      debug: 80 / 10,
+      colors: new Set()
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleSizeChange = this.handleSizeChange.bind(this)
@@ -82,49 +85,30 @@ class Canvas extends React.Component {
     const img = await window.createImageBitmap(imageData,0,0,prevWidth,prevHeight,{ width, height})
     ctx.drawImage(img,0,0,width,height)
     const data = ctx.getImageData(0,0,width,height)   
-    this.setState({
+    this.setState(prevState => ({
       canvasWidth: width,
       canvasHeight: height,
       imgData: data
-    })   
+    }))   
   }
 
   handleGridSizeChange(event){
     const {value, name} = event.target
-    this.setState({
+    this.setState(prevState => ({
        gridWidth: value,
        gridHeight: value,
-    })
+      //  debug: value / this.state.gridRes
+    }))
   }
 
   handleGridResChange(event){
     const {value, name} = event.target
-    this.setState({
-       gridRes: value
-    })
+    this.setState(prevState => ({
+       gridRes: value,
+      //  debug: this.state.gridWidth / value
+    }))
   }
 
-  componentDidUpdate(){    
-    // after any update
-    // 1. draw the grid
-    // 2. if we have image data, draw the image
-    this.drawGrid()
-    typeof(this.state.imgData) === 'object' && this.drawImage()    
-    if(typeof(this.state.imgData) === 'object')
-    {
-      console.log('update + imageData')  
-    } 
-    else{
-      console.log('update')  
-    }
-  } 
-  
-  componentDidMount(){
-    this.drawGrid()
-    typeof(this.state.imgData) === 'object' && this.drawImage()  
-    console.log('mount')
-}  
-  
   handleChange(event){
     const DOMURL = window.URL || window.webkitURL || window
     const img = this.image.current
@@ -150,29 +134,96 @@ class Canvas extends React.Component {
       canvas.width=iwScaled;
       canvas.height=ihScaled;
       ctx.drawImage(img,0,0,iwScaled,ihScaled);
-      // const data = ctx.getImageData(0,0,this.state.canvasWidth, this.state.canvasHeight)
       const data = ctx.getImageData(0,0,iwScaled, ihScaled)
 
-      this.setState({
+      this.setState(prevState => ({
         picture: img,        
         imgData: data,
         canvasWidth: canvas.width,
         canvasHeight: canvas.height
-      })
+      }))
       DOMURL.revokeObjectURL(url)
-      this.drawGrid()
-      this.drawImage()
     }
   }
+
+  componentDidUpdate(prevProps, prevState){       
+    // after any update
+    // 1. draw the grid
+    // 2. if we have image data, draw the image
+    this.copyPixels()
+    // if(typeof(this.state.imgData) === 'object')
+    // {
+    //   console.log('update + imageData')  
+    // } 
+    // else{
+    //   console.log('update')  
+    // }
+  } 
+  
+  componentDidMount(){
+    this.drawGrid()
+    typeof(this.state.imgData) === 'object' && this.drawImage()  
+    console.log('mount')
+}  
 
   drawImage(){
     const canvas = this.canvas.current
     const ctx = canvas.getContext('2d')
-    ctx.putImageData(this.state.imgData, 0, 0)  
+    ctx.putImageData(this.state.imgData, 0, 0)      
+    // this.copyPixels()
   }
 
-  drawGrid(){
+  copyPixels(){    
     const canvas = this.canvas.current
+    const ctx = canvas.getContext('2d')
+    const imageData = ctx.getImageData(0,0,this.state.imgData.width,this.state.imgData.height)
+    
+    var data = imageData.data
+    const secondCanvas = this.secondCanvas.current
+    const ctx2 = secondCanvas.getContext('2d')
+
+    var res = Math.floor(this.state.gridWidth / this.state.gridRes)
+
+    var width = this.state.canvasWidth
+    var height = this.state.canvasHeight
+    var cellX = 0
+    var cellY = 0
+    var colorSet = new Set()
+
+    for (var x = 0; x <= width; x++) {
+      cellY = 0
+      if(x%res === 0) cellX ++
+      for (var y = 0; y <= height; y++) {
+        if(y%res === 0) 
+        {
+          cellY ++
+          var copy = ((res * cellY) * width + res * cellX) * 4
+          colorSet.add([data[copy], data[copy + 1], data[copy + 2]])
+        }
+
+        var index = (y * width + x) * 4
+
+        data[index] = data[copy]
+        data[index + 1] = data[copy + 1]
+        data[index + 2] = data[copy + 2]
+      }
+    }
+    var it = colorSet.values()
+    var first = it.next()
+    if(this.state.debug !== colorSet.size){
+      console.log(colorSet.size)      
+      this.drawGrid()
+      typeof(this.state.imgData) === 'object' && this.drawImage()    
+      this.setState(prevState => ({
+        debug: colorSet.size
+      }))
+    }
+    ctx2.putImageData(imageData, 0, 0)
+  }
+   
+  drawGrid(){
+    console.log("draw grid")
+    const canvas = this.secondCanvas.current
     const ctx = canvas.getContext('2d')
     ctx.clearRect(0,0,canvas.width, canvas.height)
     const w = this.state.gridWidth
@@ -283,16 +334,19 @@ class Canvas extends React.Component {
                     </Button>
                   </label> 
                 </Grid>  
+                <Grid item>
+                  <label>{this.state.debug}</label>
+                </Grid>
               </Grid>   
             </Box>
           </Grid> 
-          <Grid item xs={9}>
+          <Grid item xs={9} container direction="row">
             <Box 
               p={1} 
               border={1} 
               borderColor="grey.200" 
               borderRadius={10} 
-              minHeight={360}>
+              minHeight={360}>              
                 <canvas 
                   ref={this.canvas} 
                   width={this.state.canvasWidth} 
@@ -301,6 +355,18 @@ class Canvas extends React.Component {
                   ref={this.image} 
                   src={this.state.picture} 
                   style={hidden} />
+            </Box>
+            <Box 
+              p={1} 
+              border={1} 
+              borderColor="grey.200" 
+              borderRadius={10} 
+              minHeight={360}>    
+                <canvas 
+                  marginLeft={10}
+                  ref={this.secondCanvas} 
+                  width={this.state.canvasWidth} 
+                  height={this.state.canvasHeight} />
               </Box>
           </Grid>   
         </Grid>        
